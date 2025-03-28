5c633e7a92b36d000f99b409bbf1915dc77021bd
Devyani28 <91120643+Devyani28@users.noreply.github.com>
2025-03-28 17:35:57 +0530

    playground challenge completed

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -4,26 +4,54 @@ class StockPortfolio {
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
     // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
-        // TODO
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyingPrice = buyingPrice;
+        currentPrice = buyingPrice;
+        highestPrice = buyingPrice;
+    }
+    
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+    public double getHighestPrice() {
+        return highestPrice;
     }
 
     // TODO: Update current market price
     public void updatePrice(double newPrice) {
-        // TODO
+        if(newPrice>0){
+        this.currentPrice = newPrice;
+        if(highestPrice < newPrice){
+            highestPrice = newPrice;
+        }
+        }
     }
 
     // TODO: Calculate total profit or loss
     public double calculateProfit() {
-        // TODO
-        return 0.0;
+        return (currentPrice - buyingPrice) * quantity;;
     }
 
     // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
-        // TODO
-        return 0.0;
+        return currentPrice * quantity;
     }
 }
